6a0b859485df93665ef8424e324d83d21fb63653
Ollie Chick <och26@uclive.ac.nz>
2018-05-11 23:59:53 +1200

    Initial setup of app (following Lab 6 Ex 4)

---
+++ auction-app/src/Auctions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is a list of auctions.
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Auctions"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- auction-app/src/main.js
+++ auction-app/src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import Auctions from './Auctions.vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Auctions
+  }
+];
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+});
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
-})
+});
